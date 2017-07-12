003ec24309dc314c82ab76b13541ef9d5c611f29
Dan Turner <dan.turner@cba.com.au>
2017-07-12 15:28:44 +1000

    Change to new constellation

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -8,11 +8,10 @@ apt-get update
 apt-get install -y build-essential unzip libdb-dev libsodium-dev zlib1g-dev libtinfo-dev solc sysvbanner wrk
 
 # install constellation
-wget -q https://github.com/jpmorganchase/constellation/releases/download/v0.0.1-alpha/ubuntu1604.zip
-unzip ubuntu1604.zip
-cp ubuntu1604/constellation-node /usr/local/bin && chmod 0755 /usr/local/bin/constellation-node
-cp ubuntu1604/constellation-enclave-keygen /usr/local/bin && chmod 0755 /usr/local/bin/constellation-enclave-keygen
-rm -rf ubuntu1604.zip ubuntu1604
+wget -q https://github.com/jpmorganchase/constellation/releases/download/v0.1.0/constellation-0.1.0-ubuntu1604.tar.xz
+tar --xz -xvf constellation-0.1.0-ubuntu1604.tar.xz
+cp constellation-0.1.0-ubuntu1604/constellation-node /usr/local/bin && chmod 0755 /usr/local/bin/constellation-node
+rm -rf constellation-0.1.0-ubuntu1604.tar.xz constellation-0.1.0-ubuntu1604
 
 # install golang
 GOREL=go1.7.3.linux-amd64.tar.gz
